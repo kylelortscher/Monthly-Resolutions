8cde7767dbf95e9d0ac57d4d06dedfc0e28841f0
Kyle Lortscher <kylelortscher@gmail.com>
2015-08-18 12:23:19 -0700

    Added Checkbox With Cross Out

--- resolutions.js
+++ resolutions.js
@@ -6,6 +6,29 @@ if (Meteor.isClient) {
       return Resolutions.find();
     }
   });
+  Template.body.events({
+    'submit .new-resolution': function(event) {
+      var title = event.target.title.value;
+
+      Resolutions.insert({
+        title : title,
+        createdAt: new Date()
+      });
+
+      event.target.title.value = ""
+
+      return false;
+    }
+  });
+
+  Template.resolution.events({
+    'click .delete': function() {
+      Resolutions.remove(this._id);
+    },
+    'click .toggle-checked': function() {
+      Resolutions.update(this._id, {$set: {checked: !this.checked}})
+    }
+  });
 }
 
 if (Meteor.isServer) {
